78705712a2b6e625e46abfecfae11b0b89ddc8d8
Zar <zardronpesquera1968@gmail.com>
2021-05-03 11:20:21 +0800

    Initial Commit

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://angelo047:poslaklopq123@cluster0.z8hin.mongodb.net/devconnector?retryWrites=true&w=majority'
+};
--- server.js
+++ server.js
@@ -1,34 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const env = require('dotenv');
+
 const app = express();
 
-const Users = require('./routes/api/Users');
-const Profile = require('./routes/api/Profile');
-const Post = require('./routes/api/Posts');
+// DB Config
+const db = require('./config/keys').mongoURI;
 
+// Connect to MongoDB
+mongoose.connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err))
 
-// Routes
-env.config();
+app.get('/', (req, res) => res.send("Hello"));
 
-// DB Connect
-mongoose.connect(
-    `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.z8hin.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`,
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true
-    }
-).then(() => {
-    console.log('Database Connected');
-});
+const port = process.env.PORT || 5000;
 
-// Use Routes
-app.use('/api/users', Users);
-app.use('/api/profile', Profile);
-app.use('/api/posts', Post);
-
-// Port
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
-});
+app.listen(port, () => console.log(`Server is running on port ${port}`));
