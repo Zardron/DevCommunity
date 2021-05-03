d7fe3412799e5d40f39afb547d1d007e7cce3236
Zar <zardronpesquera1968@gmail.com>
2021-05-03 15:21:05 +0800

    Adding backend code for login and register with jwt

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://angelo047:poslaklopq123@cluster0.z8hin.mongodb.net/devconnector?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://angelo047:poslaklopq123@cluster0.z8hin.mongodb.net/devconnector?retryWrites=true&w=majority',
+    secretOrKey: 'secret'
 };
